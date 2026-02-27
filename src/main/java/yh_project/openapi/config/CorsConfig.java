5502628668bb05457eff86278c23252b3a149f5a
yhyoon1004 <yhyoon1004@naver.com>
2026-02-27 17:24:40 +0900

    웹 플럭스 변경, cors 설정 변경, ai 기능 추가

--- src/main/java/yh_project/openapi/config/CorsConfig.java
+++ src/main/java/yh_project/openapi/config/CorsConfig.java
@@ -1,6 +1,8 @@
 package yh_project.openapi.config;
 
 import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,9 +12,26 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 
 @Configuration
 public class CorsConfig {
+
   @Bean
   public CorsWebFilter corsWebFilter() {
-    return new CorsWebFilter(corsConfigurationSource());
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of(
+            "http://localhost:3000",
+            "https://yunhwan.kr",
+            "https://www.yunhwan.kr",
+            "https://www.renteasy.co.kr",
+            "https://renteasy.co.kr",
+            "https://dev.renteasy.co.kr"
+    ));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.addAllowedHeader("*");
+    config.setAllowCredentials(true);
+    config.setMaxAge(3600L);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return new CorsWebFilter(source);
   }
 
   @Bean
